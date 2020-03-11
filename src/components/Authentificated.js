9c69b2d742dc64bf7c225ccb166f670fdd988981
ignacioainol <iggnaxios@gmail.com>
2020-03-11 11:48:21 -0300

    jwt separate

--- src/components/Authentificated.js
+++ src/components/Authentificated.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import  {getJwt} from './../helpers/jwt';
 
 class Authenticated extends Component {
     constructor(props) {
         super(props);
-        
+
         this.state = {
             user:undefined
+        }
+    }
+
+    componentDidMount(){
+        const jwt = getJwt();
+        if(!jwt){
+            this.props.history.push('/login');
         }
     }
 
